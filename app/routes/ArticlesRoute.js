13d07bbb4790b7248381f42e9243bbe3bde4127c
Cyril Adam <cyril.adam.pro@gmail.com>
2019-09-26 11:47:03 +0200

    Refactoring ArticlesRoute
    Refactoring des catégories, Ajout des slugs et d'un route pour trouver les articles par catégories

--- app/routes/ArticlesRoute.js
+++ app/routes/ArticlesRoute.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Article = require('../models/ArticlesModel');
+var Categorie = require('../models/CategoriesModel');
 var multer = require('multer');
 var checkAuth = require('../middlewares/CheckAuth');
+var slugify = require('../utils/slugify');
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -31,8 +33,8 @@ var upload = multer({
 
 router.route('/').get((req, res, next) => {
   Article.find()
-    .select('_id titre image contenu categorie auteur')
-    .populate({ path: 'categorie', select: '_id nom'})
+    .select('_id titre image contenu categories auteur createdAt updatedAt')
+    .populate({ path: 'categories', select: '_id nom'})
     .populate({ path: 'auteur', select: '_id pseudo'})
     .exec()
     .then(docs => {
@@ -42,12 +44,17 @@ router.route('/').get((req, res, next) => {
           return {
             id: doc._id,
             titre: doc.titre,
+            slug: slugify(doc.titre),
             image: doc.image,
             contenu: doc.contenu,
-            categorie: {
-              id: (doc.categorie != null) ? doc.categorie._id : null,
-              nom: (doc.categorie != null) ? doc.categorie.nom : null,
-            },
+            createdAt: doc.createdAt,
+            updatedAt: doc.updatedAt,
+            categories: (doc.categories !== null) ? doc.categories.map(categorie => {
+              return {
+                id: (categorie != null) ? categorie._id : null,
+                nom: (categorie != null) ? categorie.nom : null,
+              }
+            }) : null,
             auteur: {
               id: (doc.auteur != null) ? doc.auteur._id : null,
               pseudo: (doc.auteur != null) ? doc.auteur.pseudo : null
@@ -66,24 +73,61 @@ router.route('/').get((req, res, next) => {
 router.route('/:id').get((req, res) => {
   let id = req.params.id;
   Article.findById(id)
-    .select('_id titre image contenu categorie auteur')
-    .populate({ path: 'categorie', select: '_id nom'})
+    .select('_id titre image contenu categories auteur createdAt updatedAt')
+    .populate({ path: 'categories', select: '_id nom'})
     .populate({ path: 'auteur', select: '_id pseudo'})
     .exec()
     .then(doc => {
       const response = {
         id: doc._id,
         titre: doc.titre,
+        slug: slugify(doc.titre),
         image: doc.image,
         contenu: doc.contenu,
-        categorie: {
-          id: (doc.categorie != null) ? doc.categorie._id : null,
-          nom: (doc.categorie != null) ? doc.categorie.nom : null,
-        },
+        createdAt: doc.createdAt,
+        updatedAt: doc.updatedAt,
+        categories: (doc.categories.length > 0) ? doc.categories.map(categorie => {
+          return {
+            id: (categorie != null) ? categorie._id : null,
+            nom: (categorie != null) ? categorie.nom : null,
+          }
+        }) : null,
         auteur: {
           id: (doc.auteur != null) ? doc.auteur._id : null,
           pseudo: (doc.auteur != null) ? doc.auteur.pseudo : null
         }
+      };
+      res.status(200).json(response);
+    })
+});
+
+router.route('/categorie/:id').get((req, res) => {
+  let categorieId = req.params.id;
+  Categorie.find({ _id: categorieId })
+    .select('_id nom')
+    .populate({
+      path: 'articles',
+      select: '_id titre image contenu auteur createdAt updatedAt',
+      populate: { path: 'auteur', select: '_id pseudo'}
+    })
+    .exec()
+    .then(doc => {
+      const response = {
+        articles: doc[0].articles.map(article => {
+          return {
+            id: article._id,
+            titre: article.titre,
+            slug: slugify(article.titre),
+            image: article.image,
+            contenu: article.contenu,
+            createdAt: article.createdAt,
+            updatedAt: article.updatedAt,
+            auteur: {
+              id: (article.auteur != null) ? article.auteur._id : null,
+              pseudo: (article.auteur != null) ? article.auteur.pseudo : null
+            }
+          }
+        })
       };
       res.status(200).json(response);
     })
@@ -96,7 +140,7 @@ router.post('/create', checkAuth, upload.single('image'), (req, res) => {
     titre: req.body.titre,
     image: 'http://localhost:4000/' + req.file.destination + req.file.filename,
     contenu: req.body.contenu,
-    categorie: req.body.categorie,
+    categories: req.body.categories,
     auteur: req.body.auteur
   },
   function (error, doc) {
@@ -106,31 +150,60 @@ router.post('/create', checkAuth, upload.single('image'), (req, res) => {
     const response = {
       id: doc._id,
       titre: doc.titre,
+      slug: slugify(doc.titre),
       image: doc.image,
       contenu: doc.contenu,
-      categorie: {
-        id: doc.categorie._id,
-      },
+      createdAt: doc.createdAt,
+      updatedAt: doc.updatedAt,
+      categories: (doc.categories.length > 0) ? doc.categories.map(categorie => {
+        return {
+          id: categorie._id,
+        }
+      }) : null,
       auteur: {
         id: doc.auteur._id,
       }
     };
+    if (doc.categories.length > 0) {
+      for(categ of doc.categories) {
+        Categorie.findById(categ, (err, categorie) => {
+          if (err) {
+            console.log(err)
+          } else {
+            categorie.articles.push(doc._id);
+            categorie.save();
+          }
+        })
+      }
+    }
     res.status(200).json(response);
   })
 });
 
-router.put('/:id', checkAuth, (req, res) => {
+router.put('/:id', checkAuth, upload.single('image'), (req, res) => {
   let id = req.params.id;
   Article.findById(id, (err, article) => {
     if (err) {
       console.log(err);
     } else {
       article.titre = req.body.titre;
-      article.image = req.body.image;
+      if (req.file !== undefined) article.image = 'http://localhost:4000/' + req.file.destination + req.file.filename;
       article.contenu = req.body.contenu;
-      article.categorie = req.body.categorie;
+      article.categories = req.body.categories;
       article.auteur = req.body.auteur;
       article.save().then( article => {
+        if (article.categories.length > 0) {
+          for(categ of article.categories) {
+            Categorie.findById(categ, (err, categorie) => {
+              if (err) {
+                console.log(err)
+              } else {
+                categorie.articles.push(article._id);
+                categorie.save();
+              }
+            })
+          }
+        }
         res.json('Article mis à jour');
       }).catch(err => {
         res.status(400).send('Erreur lors de l\'enregistrement');
